4d38d80122074cb06b11ed1809ba16ff5d87602d
fux <moutain.xiu@gmail.com>
2017-05-12 15:21:47 +0800

    delet build dir

--- build/CMakeFiles/lcmtypes_lcm_scan.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "lib/liblcmtypes_lcm_scan.a"
-)
--- build/include/lcmtypes/lcm_scan.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __lcmtypes_lcm_scan_hpp__
-#define __lcmtypes_lcm_scan_hpp__
-
-#include "lidar/lidar_t.hpp"
-#include "exlcm/example_t.hpp"
-
-#endif
